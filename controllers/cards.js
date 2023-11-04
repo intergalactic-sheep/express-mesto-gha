2e456fc69f2b1fffb8368350dd107e0b30b2d2db
Egor Baranov <intergalacticsheep00@gmail.com>
2023-11-04 17:37:47 +0300

    refactoring: rewrote try and catch blocks with promises

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,67 +3,77 @@ const Card = require('../models/card');
 let statusCode = 500;
 let errorMessage = 'Ошибка на стороне сервера';
 
-module.exports.getCards = async (req, res) => {
-  try {
-    const cards = await Card.find({});
-    return res.send(cards);
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      statusCode = 400;
-      errorMessage = 'Переданы некорректные данные';
-    }
-
-    return res.status(statusCode).send({ message: errorMessage });
-  }
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send(cards))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        statusCode = 400;
+        errorMessage = 'Переданы некорректные данные';
+      }
+      res.status(statusCode).send({ message: errorMessage });
+    });
 };
 
-module.exports.createCard = async (req, res) => {
-  try {
-    const newCard = await new Card(req.body);
-    newCard.owner = req.user._id;
-    return res.status(201).send(await newCard.save());
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      statusCode = 400;
-      errorMessage = 'Переданы некорректные данные';
-    }
-
-    return res.status(statusCode).send({ message: errorMessage });
-  }
+module.exports.createCard = (req, res) => {
+  const newCard = new Card(req.body);
+  newCard.owner = req.user._id;
+  newCard
+    .save()
+    .then((createdCard) => res.status(201).send(createdCard))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        statusCode = 400;
+        errorMessage = 'Переданы некорректные данные';
+      }
+      res.status(statusCode).send({ message: errorMessage });
+    });
 };
 
-module.exports.deleteCard = async (req, res) => {
-  try {
-    const cardToDelete = await Card.findByIdAndRemove(req.params.id);
-    return res.status(200).send(cardToDelete);
-  } catch (err) {
-    if (err.name === 'CastError' && err.path === '_id') {
-      statusCode = 404;
-      errorMessage = 'Карточка с указанным ID не найден';
-    }
-
-    return res.status(statusCode).send({ message: errorMessage });
-  }
+module.exports.deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.id)
+    .then((cardToDelete) => {
+      if (!cardToDelete) {
+        statusCode = 404;
+        errorMessage = 'Карточка с указанным ID не найдена';
+        res.status(statusCode).send({ message: errorMessage });
+      } else {
+        res.status(200).send(cardToDelete);
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError' && err.path === '_id') {
+        statusCode = 404;
+        errorMessage = 'Карточка с указанным ID не найдена';
+      }
+      res.status(statusCode).send({ message: errorMessage });
+    });
 };
 
 module.exports.likeCard = (req, res) => {
-  try {
-    const likeMethod = req.method === 'PUT' ? '$addToSet' : '$pull';
-    const likedCard = Card.findByIdAndUpdate(
-      req.params.cardId,
-      { [likeMethod]: { likes: req.user._id } },
-      { new: true },
-    );
-    return res.status(200).send(likedCard);
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      statusCode = 400;
-      errorMessage = 'Переданы некорректные данные';
-    } else if (err.name === 'CastError' && err.path === '_id') {
-      statusCode = 404;
-      errorMessage = 'Карточка с указанным ID не найден';
-    }
-
-    return res.status(statusCode).send({ message: errorMessage });
-  }
+  const likeMethod = req.method === 'PUT' ? '$addToSet' : '$pull';
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { [likeMethod]: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((likedCard) => {
+      if (!likedCard) {
+        statusCode = 404;
+        errorMessage = 'Карточка с указанным ID не найдена';
+        res.status(statusCode).send({ message: errorMessage });
+      } else {
+        res.status(200).send(likedCard);
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        statusCode = 400;
+        errorMessage = 'Переданы некорректные данные';
+      } else if (err.name === 'CastError' && err.path === '_id') {
+        statusCode = 404;
+        errorMessage = 'Карточка с указанным ID не найдена';
+      }
+      res.status(statusCode).send({ message: errorMessage });
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -3,97 +3,92 @@ const User = require('../models/user');
 let statusCode = 500;
 let errorMessage = 'Ошибка на стороне сервера';
 
-module.exports.getUsers = async (req, res) => {
-  try {
-    const users = await User.find({});
-    return res.send(users);
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      statusCode = 400;
-      errorMessage = 'Переданы некорректные данные';
-    }
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        statusCode = 400;
+        errorMessage = 'Переданы некорректные данные';
+      }
 
-    return res.status(statusCode).send({ message: errorMessage });
-  }
+      res.status(statusCode).send({ message: errorMessage });
+    });
 };
 
-module.exports.getUserById = async (req, res) => {
-  try {
-    const user = await User.findById(
-      req.params.userId ? req.params.userId : req.user._id,
-    );
-    return res.send(user);
-  } catch (err) {
-    if (err.name === 'CastError' && err.path === '_id') {
-      statusCode = 400;
-      errorMessage = 'Пользователь с указанным ID не найден';
-    }
-
-    return res.status(statusCode).send({ message: errorMessage });
-  }
+module.exports.getUserById = (req, res) => {
+  const userId = req.params.userId ? req.params.userId : req.user._id;
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        statusCode = 404;
+        errorMessage = 'Пользователь с указанным ID не найден';
+        res.status(statusCode).send({ message: errorMessage });
+      } else {
+        res.send(user);
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError' && err.path === '_id') {
+        statusCode = 404;
+        errorMessage = 'Пользователь с указанным ID не найден';
+      }
+      res.status(statusCode).send({ message: errorMessage });
+    });
 };
 
-module.exports.createUser = async (req, res) => {
-  try {
-    const newUser = await new User(req.body);
-    return res.status(201).send(await newUser.save());
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      statusCode = 400;
-      errorMessage = 'Переданы некорректные данные';
-    }
-
-    return res.status(statusCode).send({ message: errorMessage });
-  }
+module.exports.createUser = (req, res) => {
+  const newUser = new User(req.body);
+  newUser
+    .save()
+    .then((createdUser) => res.status(201).send(createdUser))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        statusCode = 400;
+        errorMessage = 'Переданы некорректные данные';
+      }
+      res.status(statusCode).send({ message: errorMessage });
+    });
 };
 
-module.exports.updateUser = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const { name, about } = req.body;
-    const updatedUser = User.findByIdAndUpdate(
-      userId,
-      {
-        name,
-        about,
-      },
-      { new: true },
-    );
-    return res.status(200).send(updatedUser);
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      statusCode = 400;
-      errorMessage = 'Переданы некорректные данные';
-    } else if (err.name === 'CastError' && err.path === '_id') {
-      statusCode = 404;
-      errorMessage = 'Пользователь с указанным ID не найден';
-    }
-
-    return res.status(statusCode).send({ message: errorMessage });
-  }
+module.exports.updateUser = (req, res) => {
+  const userId = req.user._id;
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    userId,
+    { name, about },
+    { new: true, runValidators: true },
+  )
+    .then((user) => res.send({ user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        statusCode = 400;
+        errorMessage = 'Переданы некорректные данные';
+      } else if (err.name === 'CastError' && err.path === '_id') {
+        statusCode = 404;
+        errorMessage = 'Пользователь с указанным ID не найден';
+      }
+      res.status(statusCode).send({ message: errorMessage });
+    });
 };
 
-module.exports.updateUserAvatar = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const { avatar } = req.body;
-    const updatedUser = User.findByIdAndUpdate(
-      userId,
-      {
-        avatar,
-      },
-      { new: true },
-    );
-    return res.status(200).send(updatedUser);
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      statusCode = 400;
-      errorMessage = 'Переданы некорректные данные';
-    } else if (err.name === 'CastError' && err.path === '_id') {
-      statusCode = 404;
-      errorMessage = 'Пользователь с указанным ID не найден';
-    }
-
-    return res.status(statusCode).send({ message: errorMessage });
-  }
+module.exports.updateUserAvatar = (req, res) => {
+  const { userId } = req.params;
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    userId,
+    { avatar },
+    { new: true },
+  )
+    .then((updatedUser) => res.status(200).send(updatedUser))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        statusCode = 400;
+        errorMessage = 'Переданы некорректные данные';
+      } else if (err.name === 'CastError' && err.path === '_id') {
+        statusCode = 404;
+        errorMessage = 'Пользователь с указанным ID не найден';
+      }
+      res.status(statusCode).send({ message: errorMessage });
+    });
 };
